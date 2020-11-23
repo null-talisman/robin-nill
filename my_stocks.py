5716099e52179ba66a9df9f5c16253a83e3aac96
null-talisman <youngnilla@yahoo.com>
2020-11-23 12:17:36 -0500

    cleaning up stocks output, added test.py

--- my_stocks.py
+++ my_stocks.py
@@ -17,15 +17,15 @@ def main():
              password=robin_pass, 
              expiresIn=86400, 
              by_sms=True)
-    # get current info of my stocks
 
+    # get current info of my stocks
     stock_list=['TMUS','BILI','KODK','MSFT','AAPL']
     for stock in stock_list:
-        stock_info=rs.stocks.find_instrument_data(stock)
-        stock_price=rs.stocks.get_latest_price((stock), priceType=None, includeExtendedHours=True)
-        stock_quotes=rs.stocks.get_quotes(stock) 
-        print("***Current Price of " + stock + " ***")
-        print(stock_price) 
+        #stock_info=str(rs.stocks.find_instrument_data(stock))
+        stock_price=str(rs.stocks.get_latest_price((stock), priceType=None, includeExtendedHours=True))
+        stock_quotes=str(rs.stocks.get_quotes(stock))
+        stock_price_len = len(str(stock_price))
+        print(stock + ": " + "$" + stock_price[2:stock_price_len-6])
 
     ### The following code block can be enabled to test a singular symbol
     # get stock information 
---
+++ test.py
@@ -0,0 +1,12 @@
+# testing shiiiiiz
+
+def stringTest():
+    x = "['114.400000']"
+    x_len = len(x)
+    print("Stock Price (Unformatted): " + str(x))
+    print("Stock Price Length: " + str(x_len))
+    print("Formatted Price: " + x[2:x_len-6])
+
+stringTest()
+
+
