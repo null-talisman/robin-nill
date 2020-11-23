6fcab128870792475eb82642a106418df73cdf37
null-talisman <youngnilla@yahoo.com>
2020-11-23 14:45:59 -0500

    added prog.sh for full execution

---
+++ prog.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+
+# bash script to run Robinhood updates
+# 1 - update update.txt
+update_file=/home/tyler/robinhood/update.txt
+rm $update_file
+touch $update_file
+# 2 - run my_stocks.py
+python3 my_stocks.py >> update.txt
+# 3 - run mail.py 
+python3 mail.py
+
+
