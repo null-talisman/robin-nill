0729d66592775cc0e4a570b4240b77c7e186ce0f
null-talisman <youngnilla@yahoo.com>
2020-11-23 17:27:39 -0500

    updating .sh script

--- prog.sh
+++ prog.sh
@@ -4,9 +4,10 @@
 # 1 - update update.txt
 update_file=/home/tyler/robinhood/update.txt
 rm $update_file
-touch $update_file
+# touch $update_file
+# chmod 666 $update_file
 # 2 - run my_stocks.py
-python3 my_stocks.py >> update.txt
+python3 my_stocks.py > update.txt
 # 3 - run mail.py 
 python3 mail.py
 
