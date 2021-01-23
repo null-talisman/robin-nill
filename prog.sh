733ac29206097ef83a99a450948d8ab8cf210e69
null-talisman <youngnilla@yahoo.com>
2021-01-23 14:53:24 -0500

    fixed shell script!

--- prog.sh
+++ prog.sh
@@ -1,20 +1,16 @@
 #! /bin/bash
 
-# bash script to run Robinhood updates
-# 1 - update update.txt
+# bash script to run Robinhood updates.
+# file locations 
 update_file=/home/tyler/robinhood/update.txt
-if [ -f $update_file ]
-   then 	
-      rm $update_file
-      touch $update_file
-      chmod 666 $update_file
-fi
-# 2 - run my_stocks.py
-python3 /home/tyler/robinhood/my_stocks.py > update.txt
-# 3 - run mail.py 
-# python3 /home/tyler/robinhood/mail.py
-
-# return update.txt content
-cat update.txt
+main_file=/home/tyler/robinhood/my_stocks.py
+# run my_stocks.py
+python3 $main_file
+# probably amateur-hour right here
+rm -r $update_file
+touch $update_file
+chmod 777 $update_file
+# re-run my_stocks.py and update update.txt
+python3 $main_file > $update_file
 
 
