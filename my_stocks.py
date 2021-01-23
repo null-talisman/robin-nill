09e2adea92cce3438fb79210514d1a1e1ebfad4e
null-talisman <youngnilla@yahoo.com>
2021-01-23 15:05:17 -0500

    cleaning up comments. removed 2 unused variables

--- my_stocks.py
+++ my_stocks.py
@@ -1,14 +1,15 @@
 """
+Robinhood Trading Assistant
+@null-talisman
+
 1. Get current prices of stocks entered in stocks.txt file
 2. Compare these current prices against old prices stored in update.txt
     - If a significant change is detected, write the change info to news.txt
-        and email the user.
-        - replace old price data with new data in update.txt 
-    - If no significant change is detected, just update the prices in update.txt
+        and then:
+        - run mail.py to email info from news.txt to specified user. 
+    - If no significant change is detected, the current prices will be updated 
+      in the update.txt file via a bash script.
 """
-
-# using robin stocks for robinhood 
-# null-talisman 
 
 # imports 
 import os 
@@ -40,34 +41,23 @@ def main():
     getStockPrices(STOCK_NAMES)
     # get old price of stock stored in update.txt
     getOldPrices()
-    #os.remove("update.txt")
-    #newUpdate = open("update.txt", "w+")
-    # display our lists of new and old prices 
-    #print("-----NEW PRICES-----")
-    #print(NEW_STOCK_PRICES)
-    #print("-----OLD PRICES-----")
-    #print(OLD_STOCK_PRICES)
     # compare the lists and return % inc/dec
     comparePrices()
 
 # get prices of stocks stored in a list, write to update.txt
 def getStockPrices(stocks):
-    #priceFile = open("update.txt", "w")
-    #fileText = priceFile.read().splitlines()
+    # for each stock in list of stocks
     for stock in stocks:
-        #stock_info=str(rs.stocks.find_instrument_data(stock))
+        # get latest price for stock
         stock_price=str(rs.stocks.get_latest_price((stock), priceType=None, includeExtendedHours=True))
-        stock_quotes=str(rs.stocks.get_quotes(stock))
         stock_price_len = len(str(stock_price))
         print(stock + ": " + "$" + stock_price[2:stock_price_len-6])
         NEW_STOCK_PRICES.append(stock_price[2:stock_price_len-6])
-        #print(stock_price[2:stock_price_len-6])
 
 # get old prices from update.txt and append them to old price list
 def getOldPrices():
     priceFile = open("update.txt", "r")
     oldPriceInfo = priceFile.read().splitlines()[4:]
-    #print("******* OLD *********")
     for line in oldPriceInfo:
         # prices are stored as 'TSLA: $845.46' so we need to find '$' and start from there
         index = line.find('$')
@@ -78,6 +68,7 @@ def getOldPrices():
 
 # we have a list of current prices and old prices. compare them. 
 def comparePrices():
+    now = datetime.datetime.now()
     sigChange = False
     index = 1
     listLength = len(OLD_STOCK_PRICES)
@@ -91,20 +82,10 @@ def comparePrices():
         change = 100 - (new / old * 100)
         prcntChange = float(format(change, '.2f'))
         if (prcntChange > 5.0) or (prcntChange < -5.0):
-            #print("Significant Change Detected for: " + name)
-            #print(prcntChange)
             sigChange = True
-            changeFile = open("news.txt", "w")
-            newNews.write("Significant change detected\nStock: " + str(name) + "\nCurrent Price: " + str(new) + "\nOld Price: " + str(old) + "\nChange: %" + str(prcntChange) + "\n")
+            newNews.write("Significant change detected at " + now.strftime('%H:%M:%S') + "\nStock: " + str(name) + "\nCurrent Price: " + str(new) + "\nOld Price: " + str(old) + "\nChange: %" + str(prcntChange) + "\n")
             # significant change in price detected, send an email with news.txt to user
         index+=1
-        # we've checked our prices, now update the update.txt file
-    
-    #print(length)
-    #for i in NEW_STOCK_PRICES:
-    #    new = NEW_STOCK_PRICES[i]
-    #    old = OLD_STOCK_PRICES[i]
-    #    print("Current Price: " + new + "\nOld Price: " + old)
 
 # login() function for robinhood login
 def login():
