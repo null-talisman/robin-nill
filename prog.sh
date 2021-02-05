2fb9e57d927d72ef81a3bd12ad119c0b41e454b8
null-talisman <youngnilla@yahoo.com>
2021-02-05 14:00:32 -0500

    fixed sh script for cron

--- prog.sh
+++ prog.sh
@@ -2,15 +2,8 @@
 
 # bash script to run Robinhood updates.
 # file locations 
-update_file=/home/tyler/robinhood/update.txt
 main_file=/home/tyler/robinhood/my_stocks.py
 # run my_stocks.py
-python3 $main_file
-# probably amateur-hour right here
-rm -r $update_file
-touch $update_file
-chmod 777 $update_file
-# re-run my_stocks.py and update update.txt
-python3 $main_file > $update_file
+python3 $main_file 
 
 
